1186509bd0f3ec5e7c88d2fc42721bf3b4186d7c
Alexis Prevost-Maynen <killer76520@gmail.com>
2018-10-18 19:12:53 +0200

    stiky

--- js/index.js
+++ js/index.js
@@ -11,7 +11,6 @@ $(window).on('load', function() {
 		let data = $(this).attr('data-tab');
 
 		if(data != undefined) {
-			console.log(data);	
 
 			$('main').removeClass();
 
@@ -22,4 +21,23 @@ $(window).on('load', function() {
 			}
 		}
 	});
+
+	if($('main').hasClass('agenda')) {
+
+		const offsetTopMessageRead = $('.wrapper-tab-agenda .message-interaction .message-read').offset().top;
+		const heightMessageRead    = $('.wrapper-tab-agenda .message-interaction .message-read').height();
+
+		$(window).scroll(function(){
+			let scrollBottom = $(window).scrollTop() + $(window).height();
+
+		    if ( scrollBottom >= (offsetTopMessageRead + heightMessageRead + 30)  ) {
+		    	$('.wrapper-tab-agenda .message-interaction .message-read').addClass('sticky');
+		    	let left = $('.wrapper-tab-agenda .message-interaction .messages-action').offset().left + 100;
+		    	$('.wrapper-tab-agenda .message-interaction .message-read').css("right", left + 'px');
+		    }
+		    else {
+		    	$('.wrapper-tab-agenda .message-interaction .message-read').removeClass('sticky');
+		    }
+		})
+	}
 })
