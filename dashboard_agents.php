75923a39549659b526d5c84b063750e3f114be49
Alexis Prevost-Maynen <killer76520@gmail.com>
2018-10-19 18:54:23 +0200

    Dashbord - en cours

--- dashboard_agents.php
+++ dashboard_agents.php
@@ -97,5 +97,47 @@
 				</div>
 			</div>
 		</div>
+		<div class="el el-style-1">
+			<div class="head head-2">
+				<h4>Rendez-vous repporté / En attente</h4>
+				<div class="container-dropdown">
+					<div class="container-rslt">
+						<div class="rslt">
+							Janvier 2018
+						</div>
+						<div class="arrow"></div>
+						<div class="dropdown">
+							
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="container-ist container-ist-2">
+				<div class="col">
+					<div class="container-num bullet blue">
+						<div class="num-prct">71%</div>
+						<div class="num-lgd">Rendez vous reportés</div>
+						<div class="graph">
+							<span>+2%</span>
+							<img src="img/agents/common/graph.svg" alt="">
+						</div>
+					</div>
+					<div class="container-num bullet gray">
+						<div class="num-prct">29%</div>
+						<div class="num-lgd">Rendez vous en attente</div>
+						<div class="graph neg">
+							<span>-4%</span>
+							<img src="img/agents/common/graph-neg.svg" alt="">
+						</div>
+					</div>
+				</div>
+				<div class="container-circle">
+					<img src="img/agents/dashboard/oval.svg">
+					<svg viewBox="0 0 102 102" xmlns="http://www.w3.org/2000/svg">
+						<circle class="path" cx="599" cy="503" r="50" transform="translate(-548 -452)" stroke="#5fb2c4" fill="none" fill-rule="evenodd"/>
+					</svg>
+				</div>
+			</div>
+		</div>
 	</div>
 </div>
