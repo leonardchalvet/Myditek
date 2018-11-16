5a2bd54c8028463c2cc65c9d83999d8b0bbb0550
Alexis Prevost-Maynen <killer76520@gmail.com>
2018-11-16 10:59:02 +0100

    Page 404

---
+++ page404.php
@@ -0,0 +1,30 @@
+<html>
+	<head>
+
+		<title>404</title>
+
+		<meta name="description" content="" />
+
+		<meta http-equiv="content-type" content="text/html; charset=utf8" />
+
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+		<link rel="stylesheet" type="text/css" href="style/css/home.css">
+
+		<script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
+
+	</head>
+	
+	<body>
+
+		<div class="wrapper-tab-404">
+			<div class="title">404</div>
+
+			<div class="text">Sorry, page not found</div>
+
+			<a href="" class="button">Home</a>
+
+			<img src="img/common/illustration404.svg">
+		</div>
+	</body>
+</html>
